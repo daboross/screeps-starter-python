fc3ae2254e7281ebd6151dd1bbf58b794a60add8
Dabo Ross <daboross@daboross.net>
2017-02-23 23:28:46 -0800

    Update Gruntfile to use the 'default' branch by default, as 'v2' is probably not needed/expected!

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,7 @@ module.exports = function (grunt) {
                 // Trim right in order to remove any newlines at the end of the files.
                 email: grunt.file.read('./.screeps-email').trimRight(),
                 password: grunt.file.read('./.screeps-password').trimRight(),
-                branch: grunt.option('branch') || 'v2',
+                branch: grunt.option('branch') || 'default',
                 ptr: grunt.option('ptr') || false
             },
             dist: {
